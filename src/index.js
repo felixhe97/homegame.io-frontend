906e09552d7e4ee144d2edd2d914010ffa379f79
Felix He <felixhe97@gmail.com>
2020-05-27 14:11:26 -0700

    refactored file directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.js";
+
+import "./index.css";
+
+import Room from "./Room.js";
+
+class App extends React.Component {
+    render() {
+        return (
+            <React.StrictMode>
+                <div className="App">
+                    <Room />
+                </div>
+            </React.StrictMode>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
