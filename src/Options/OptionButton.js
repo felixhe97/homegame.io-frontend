2f76ea8e9e95f7d0448f2ed7b15e890eb634fb4d
Felix He <felixhe97@gmail.com>
2020-05-30 23:51:20 -0700

    top right option based on player seating, general options

---
+++ src/Options/OptionButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OptionButton(props) {
+    return (
+        <button type="button" className="Option">
+
+        </button>
+    );
+}
+
+export default OptionButton;
---
+++ src/Options/Options.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import OptionButton from './OptionButton.js';
+
+class Options extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Options;
